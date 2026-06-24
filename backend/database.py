cebc6c25a68f4820c3fa109686d4d6fc5d10058f
Lakshya Karn <282327699+lakshyakarn10@users.noreply.github.com>
2026-06-25 02:22:15 +0530

    Changed db_url to get from os.getenv

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-db_url="postgresql://postgres:12345@localhost:5432/inventory_db"
+db_url=os.getenv("DATABASE_URL")
 engine=create_engine(db_url)
 
 SessionLocal= sessionmaker(
